a9a56ef1d42f59c888b957630576eb28602b7644
Nahye Park <ellapark@naver.com>
2021-11-27 04:27:28 +0000

    test commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,68 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller, JSONModel) {
+		"use strict";
+
+		return Controller.extend("iitp.zday1304.controller.View1", {
+			onInit: function () {
+                let oModel = new JSONModel();
+                oModel.loadData("../model/data.json");
+                this.getView().setModel( oModel );
+               
+                let oData = {
+                    "expanded" : true
+                };
+                let oModel2 = new JSONModel();
+                oModel2.setData( oData );
+                this.getView().setModel( oModel2 , "oModel2" );
+			},
+            onPressAir:function (oEvt) {
+                let sPath = oEvt.getSource().getBindingContext().getPath();
+                // alert(sPath);
+                let oTableCon = this.byId("idTableCon");
+                oTableCon.bindElement( sPath );
+                // alert( sPath );
+            },
+            onPressCon:function (oEvt){
+                let sPath = oEvt.getSource().getBindingContext().getPath();
+                let oTablePanel = this.byId("idPanel");
+                oTablePanel.bindElement( sPath );
+                // alert( sPath );
+
+                // let sPathExpandable = oEvt.getSource().getBindingContext().getPath();
+                // oTablePanel.bindElement( sPathExpandable );
+                
+                // oTablePanel.setExpanded(true);
+                // oTablePanel.setExpanded(false);
+                // let oExpanded = oTablePanel.getExpanded();
+                // // alert(oExpanded);
+
+                // oTablePanel.setExpanded(!oExpanded);
+                // if(oExpanded) {
+                //     // let oPanel = new sap.m.Panel(); 
+
+                //     oTablePanel.attachExpand(
+                //         function(){
+                //         oTablePanel.setExpanded(false);
+                //     });
+                    
+                }
+            },
+            onSelect:function(oEvent) {
+                let sPath = oEvent.getParameter("selectedItem").getBindingContext().getPath();
+                // alert(sPath);
+                let oCombo = this.byId("idTableCon");
+                // alert(oCombo);
+                oCombo.bindElement( sPath );
+                // let selectKey = oCombo.getSelectedKey();
+                // alert(selectKey);
+                // oCombo.bindElement( sPath );
+                // alert(sPath);
+            }
+		});
+	});
